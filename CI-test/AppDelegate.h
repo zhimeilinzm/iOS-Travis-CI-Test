4bf3471200c3c3cd80dfd5b35d47963773056ad4
linzhimin <linzm@zhimei360.com>
2016-03-09 11:18:40 +0800

    Initial Commit

---
+++ CI-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CI-test
+//
+//  Created by 林智敏 on 16/3/9.
+//  Copyright © 2016年 林智敏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CI-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CI-test
+//
+//  Created by 林智敏 on 16/3/9.
+//  Copyright © 2016年 林智敏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
